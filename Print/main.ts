5c445525ab25d2c61be837695ce209981316c2cf
Abdul Ahad Laiq <umer.laiq123@gmail.com>
2024-09-18 21:34:58 +0500

    Pakistan

--- Print/main.ts
+++ Print/main.ts
@@ -2,4 +2,5 @@ console.log("hello world");
 console.log("I am taking governor classes");
 console.log("It is for AI");
 console.log("Ai is the future of all");
-console.log("best of luck");    
+console.log("best of luck");   
+console.log("Pakistan Zindabad") 
