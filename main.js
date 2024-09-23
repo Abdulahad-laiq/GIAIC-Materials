c6329396e70c6c46b3bdb4f662edbddf363d5331
Abdul Ahad Laiq <umer.laiq123@gmail.com>
2024-09-23 22:54:28 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var num1 = 2;
+var num2 = 3;
+console.log(num1 + num2);
+var message = "Abdul Ahad";
+console.log(message);
+var a = 2;
+var b = 4;
+console.log(b / a);
+console.log("hellow world");
